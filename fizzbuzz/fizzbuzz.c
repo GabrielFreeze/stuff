4470329a8034921ff4890e686dc5d359ce74981e
Gabriel_Freeze <81355262+GabrielFreeze@users.noreply.github.com>
2022-11-30 11:10:57 +0100

    Update fizzbuzz.c

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -2,15 +2,12 @@
 #include <inttypes.h>
 
 int main() {
-
     for (int i=1 ; i<=100; i++) {
-
       uint32_t Fizz[4] = {i,0x2A447,0x2A51F,0x2A51A447};
       uint8_t Buzz = ((!(i%5)) << 1) | (!(i%3));
       
       if (!Buzz) printf("%d\n",Fizz[Buzz]);
       else while(Fizz[Buzz]>>=3) putc(0xA75427A6946 >> ((Fizz[Buzz]&7)<<0B11) & 255ULL,stdout);
     }
-
     return 0;
 }
